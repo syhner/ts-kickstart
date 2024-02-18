fc5be3284eace9e5ccc8fd4e5275284f62e20ae3
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2024-02-18 11:27:46 +0100

    restore conditional for not using local db on edge runtime

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -7,7 +7,7 @@ import * as moviesSchemas from './schemas/movies';
 export const schema = { ...moviesSchemas };
 
 const config: Parameters<typeof createClient>[0] =
-	env.NODE_ENV === 'production'
+	env.NODE_ENV === 'production' || env.NEXT_RUNTIME === 'edge'
 		? {
 				url: z.string().parse(env.DB_URL),
 				authToken: env.DB_TOKEN,
