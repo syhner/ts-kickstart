6b182fa6b1cbb04f4bd7b9d3da85d02c44bfd7e6
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2024-02-18 10:51:02 +0100

    don't use local db when using nextjs edge

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,14 +1,13 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { z } from 'zod';
 import * as moviesSchemas from './schemas/movies';
-
-export const schema = { ...moviesSchemas };
-
 import { createClient } from '@libsql/client';
 import { env } from 'env';
 
+export const schema = { ...moviesSchemas };
+
 const config: Parameters<typeof createClient>[0] =
-	env.NODE_ENV === 'production'
+	env.NODE_ENV === 'production' || env.NEXT_RUNTIME === 'edge'
 		? {
 				url: z.string().parse(env.DB_URL),
 				authToken: env.DB_TOKEN,
